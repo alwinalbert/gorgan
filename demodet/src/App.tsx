1da9f7fdbc4869f6a4e51fe439a35f63d2699b67
Alwiii <alwinalbert70@gmail.com>
2025-12-17 19:36:13 +0530

    testt

--- demodet/src/App.tsx
+++ demodet/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         </div>
 
         <h1 className="text-5xl font-bold text-center mb-8 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-          Vite + React
+          demodet
         </h1>
 
         <div className="text-center mb-6">
